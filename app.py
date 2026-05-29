a1292d44d936de4dd541fea4523b59471aeac4a7
P Nikhil Kumar Naik <pnikhilkumarnaik@gmail.com>
2026-05-29 13:19:31 +0530

    updated

--- app.py
+++ app.py
@@ -35,8 +35,11 @@ docsearch = PineconeVectorStore.from_existing_index(index_name=index_name,
 PROMPT=PromptTemplate(template=prompt_template,input_variables=["context","question"])
 chain_type_kwargs={"prompt":PROMPT}
 
-llm = ChatGroq(model="Gemma2-9b-It",  
-       api_key=GROQ_API_KEY, temperature=0.8 )
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=GROQ_API_KEY,
+    temperature=0.8
+)
 
 
 qa=RetrievalQA.from_chain_type(
